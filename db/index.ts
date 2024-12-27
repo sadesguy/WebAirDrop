b8eaa4458b7dfe61bdf92641a9f29ac4a95e4415
sadesguy <stas@loutchansky.com>
2024-12-27 16:38:04 +0400

    fix: bun run dev not working

--- db/index.ts
+++ db/index.ts
@@ -4,7 +4,7 @@ import * as schema from "@db/schema";
 
 if (!process.env.DATABASE_URL) {
   throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
+    "DATABASE_URL must be set. Did you forget to provision a database?"
   );
 }
 
